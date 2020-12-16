bb312475b26b20479bf12962dccb93db7769e723
Campbell <campbell.pryde@xbrl.us>
2020-12-15 21:56:44 -0500

    Added Taxonomy Validation Checks

---
+++ # compile.sh
@@ -0,0 +1,5 @@
+# compile
+python3 Arelle-master/arellecmdline.py --xule-compile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/source  --plugins "xule" --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip
+
+
+python3 Arelle-master/arellecmdline.py --plugins 'xule' -f /Users/campbellpryde/Documents/GitHub/FERC-TaggingGuidance/Instance/Sample_Form_1-Small.xbrl -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/Form1.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip
