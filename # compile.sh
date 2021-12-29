f2783e5b8a9c0ead0ec0c1d8ad0f4fabac4bf369
Campbell <campbell.pryde@xbrl.us>
2021-12-29 14:17:19 -0500

    Taxonomy Dashboard changes

--- # compile.sh
+++ # compile.sh
@@ -1,7 +1,38 @@
 # compile
-python3 Arelle-master/arellecmdline.py --xule-compile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/source  --plugins "xule" --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip
+/Applications/Arelle.app/Contents/MacOS/arelleCmdLine  --xule-compile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/source  --plugins "xule" --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip
 
 
-python3 Arelle-master/arellecmdline.py --plugins 'xule' -f /Users/campbellpryde/Documents/GitHub/FERC-TaggingGuidance/Instance/Sample_Form_1-Small.xbrl -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/Form1.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip
+python3 Arelle-master/arellecmdline.py --plugins 'xule|transforms/SEC' -f '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/blank-instance/ixbrl-viewer_extracted.xbrl' -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/SASB3.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip --packages '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Draft SASB Taxonomy 2021-01-18.zip'
 
-python3 Arelle-master/arellecmdline.py --plugins 'xule|transforms/SEC' -f '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Novo Nordisk v2 - 2020.12.07/ixbrl-viewer.htm' -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/SASB2.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip --packages '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Draft SASB Taxonomy 2020-12-07.zip'
+
+python3 Arelle-master/arellecmdline.py --plugins 'xule|transforms/SEC' -f '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Novo Nordisk v2 - 2021-01-18/ixbrl-viewer_extracted.xbrl' -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/SASB3.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip --packages '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Draft SASB Taxonomy 2020-12-07.zip'
+
+
+python3 Arelle-master/arellecmdline.py --plugins 'xule' -f '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/blank-instance/ixbrl-viewer_extracted.xbrl' -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/SASB4.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip --packages '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Draft SASB Taxonomy 2020-12-07.zip'
+
+python3 Arelle-master/arellecmdline.py --plugins 'xule|transforms/SEC' -f '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Novo Nordisk v2 - 2020.12.07/ixbrl-viewer.htm' -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/SASB4.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip --packages '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Draft SASB Taxonomy 2020-12-07.zip'
+
+python3 Arelle-master/arellecmdline.py --plugins 'xule|transforms/SEC' -f '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Novo Nordisk v2 - 2020.12.07/ixbrl-viewer_extracted.xbrl' -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/SASB4.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip --packages '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Draft SASB Taxonomy 2020-12-07.zip'
+
+
+python3 Arelle-master/arellecmdline.py --plugins 'xule|transforms/SEC' -f '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Draft SASB Taxonomy 2021-02-02.zip' -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/SASB4.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip --packages '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Draft SASB Taxonomy 2021-02-02.zip'
+
+
+
+
+
+/Applications/Arelle.app/Contents/MacOS/arelleCmdLine --plugins 'xule|transforms/SEC' -f '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/blank-instance/ixbrl-viewer_extracted.xbrl' -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/SASB4.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip --packages '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Draft SASB Taxonomy 2021-02-02.zip'
+
+python3 Arelle-master/arellecmdline.py --plugins 'xule|transforms/SEC' -f 'xule|transforms/SEC' -f '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Novo Nordisk v2 - 2020.12.07/ixbrl-viewer_extracted.xbrl' -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/SASB4.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip --packages '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Draft SASB Taxonomy 2020-12-07.zip'
+
+/Applications/Arelle.app/Contents/MacOS/arelleCmdLine --plugins 'xule|transforms/SEC' -f 'xule|transforms/SEC' -f '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Novo Nordisk v2 - 2020.12.07/ixbrl-viewer.htm' -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/SASB4.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip --packages '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Draft SASB Taxonomy 2020-12-07.zip'
+
+
+/Applications/Arelle.app/Contents/MacOS/arelleCmdLine --plugins 'xule|transforms/SEC' -f 'xule|transforms/SEC' -f '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Novo Nordisk v2 - 2020.12.07/ixbrl-viewer.htm' -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/SASB4.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip --packages '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Draft SASB Taxonomy 2020-12-07.zip' --xule-crash
+
+/Applications/Arelle.app/Contents/MacOS/arelleCmdLine  --plugins 'xule|transforms/SEC' -f 'xule|transforms/SEC' -f '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/blank-instance/ixbrl-viewer_extracted.xbrl' -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/SASB4.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip --packages '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Draft SASB Taxonomy 2021-02-02.zip' --xule-crash
+
+
+
+
+python3 Arelle-master/arellecmdline.py --plugins 'xule|transforms/SEC' -f '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/blank-instance/ixbrl-viewer_extracted.xbrl'  -v --xule-time .005 --xule-debug --noCertificateCheck --logFile /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/output/SASB6.xml --xule-rule-set /Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/compile/dboard-ruleset.zip --packages '/Users/campbellpryde/Documents/GitHub/TaxonomyDashboard/SASB/Draft SASB Taxonomy 2021-02-02.zip' --xule-crash
